8e4baa55864f60dbfe04a6ec9bbd1b089c7c4fb1
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-02-17 12:22:45 -0300

    Aplicando diretivas *ngIf *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Student } from './student/student.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  Luke = {name: 'Luke', isJedi: true, temple: 'Coruscant'}
-  Leia = {name: 'Leia', isJedi: false}
-  Han = {name: 'Han', isJedi: false}
+
+  students: Student[] = [
+    { name: 'Luke', isJedi: true, temple: 'Coruscant' },
+    { name: 'Leia', isJedi: false },
+    { name: 'Han', isJedi: false }
+  ]
 }
